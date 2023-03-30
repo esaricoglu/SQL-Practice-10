d861e37c9ee06d83bacf6f9a192442d050aaecc3
esaricoglu <84877085+esaricoglu@users.noreply.github.com>
2023-03-31 01:31:09 +0300

    Add files via upload

---
+++ 3.sql
@@ -0,0 +1,6 @@
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name
+-- isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+
+SELECT rental.rental_id,customer.first_name,customer.last_name FROM rental
+FULL JOIN customer
+ON rental.customer_id = customer.customer_id;
